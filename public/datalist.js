037606764e729e4bbfc14b4c75ae191ee8f2f053
muath-khatib <mo3az.khatib@hotmail.com>
2021-08-04 17:27:07 +0300

    commit edite's

--- public/datalist.js
+++ public/datalist.js
@@ -1,15 +1,14 @@
-const headerTtitle = document.querySelector('.header-title')
-const dataListSection = document.querySelector('#list')
+const headerTtitle = document.querySelector('.header-title');
+const dataListSection = document.querySelector('#list');
 // eslint-disable-next-line no-unused-vars
-const createDataList = (data)=>{
+const createDataList = (data) => {
   url = `/search?${data}`;
-  sendRequest(url,(data)=>{
-          data.forEach((elm)=> {
-            const createOption = document.createElement('option');
+  sendRequest(url, (data) => {
+    data.forEach((elm) => {
+      const createOption = document.createElement('option');
       dataListSection.appendChild(createOption);
-      createOption.value = `${elm.name} , ${elm.country}` ;
+      createOption.value = `${elm.name} , ${elm.country}`;
+    });
   });
-  });
-    // Update the placeholder text.
-    headerTtitle.textContent = "Loading options...!";
+  headerTtitle.textContent = 'Loading options...!';
 };
